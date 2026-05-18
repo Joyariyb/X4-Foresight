f65a67f40fc301055c7fe7cb2c4275074a2a5ef6
Joyariyb <joyariyb@gmail.com>
2026-05-19 09:37:43 +1000

    File structure cleanup

--- Legacy/Legacy/test_ui.py
+++
@@ -1,7 +0,0 @@
-import webview
-
-# Create the window
-window = webview.create_window('X4 Save Data Tool Test', 'https://flowrl.com')
-
-# Explicitly use the 'qt' engine to keep the window open
-webview.start(gui='qt')
